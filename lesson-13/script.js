30fded241f88309b6699b8c58f5d60c80546781b
omarsapu <omarrsapumah@mail.ru>
2026-08-23 15:01:35 +0300

    Add files via upload

---
+++ lesson-13/script.js
@@ -0,0 +1,24 @@
+// Задача 1
+const firstName = "Алексей";
+const lastName = "Иванов";
+const isStudent = true;
+
+// Задача 2
+const age = 21;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+// Задача 3
+console.log(
+  `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`,
+);
+
+// Задача 4
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+
+// 123456789falsetrye 
