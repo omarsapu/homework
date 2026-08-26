1fdb1b10f770e03e2a476399dc736588eb7883b3
omarsapu <omarrsapumah@mail.ru>
2026-08-26 21:31:18 +0300

    Add files via upload

---
+++ lesson-15/main.js
@@ -0,0 +1,28 @@
+// Задача 1
+for (i = 1; i <= 20; i++) {
+  if (i % 4 == 0) {
+    console.log(i);
+  }
+}
+
+// Задача 2
+const number = prompt("Ведите число:");
+let factorial = 1;
+
+for (i = 1; i <= number; i++) {
+  factorial = factorial * i;
+}
+console.log(factorial);
+
+// Задача 3
+for (i = 1; i <= 8; i++) {
+  let line = "";
+  for (j = 1; j <= 8; j++) {
+    if ((i + j) % 2) {
+      line += "Б"
+    } else {
+      line += "Ч"
+    }
+  }
+  console.log(line)
+}
